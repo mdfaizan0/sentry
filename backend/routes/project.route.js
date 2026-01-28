8b10cb34fb59871e32549d9e45b39bdf20567b70
mdfaizan0 <faizanm78692@gmail.com>
2026-01-28 23:19:20 +0530

    fix: project route for accept/reject

--- backend/routes/project.route.js
+++ backend/routes/project.route.js
@@ -5,21 +5,20 @@ import { checkProjectAccess } from "../middlewares/project.middleware.js"
 
 const router = express.Router()
 
+router.get("/", protect, listProjects)
+router.get("/:projectId", protect, checkProjectAccess, getOneProject)
+router.post("/", protect, createProject)
+router.put("/:projectId", protect, checkProjectAccess, updateProject)
+router.delete("/:projectId", protect, checkProjectAccess, deleteProject)
+
+router.post("/:projectId/add-member", protect, checkProjectAccess, addMember)
+router.post("/:projectId/invite-member", protect, checkProjectAccess, addMemberByInvite)
+router.post("/:projectId/remove-member", protect, checkProjectAccess, removeMember)
+
+
+router.get("/invites/all/:projectId", protect, checkProjectAccess, getInvites)
+
 router.get("/invite/accept", acceptInvite)
 router.get("/invite/reject", rejectInvite)
-
-router.use(protect)
-
-router.get("/", listProjects)
-router.get("/:projectId", checkProjectAccess, getOneProject)
-router.post("/", createProject)
-router.put("/:projectId", checkProjectAccess, updateProject)
-router.delete("/:projectId", checkProjectAccess, deleteProject)
-
-router.post("/:projectId/add-member", checkProjectAccess, addMember)
-router.post("/:projectId/invite-member", checkProjectAccess, addMemberByInvite)
-router.post("/:projectId/remove-member", checkProjectAccess, removeMember)
-
-router.get("/invites/all/:projectId", checkProjectAccess, getInvites)
 
 export default router
